7cabf358edcd3252a333a330e92987c5f8cc2d04
Eduardo <educonts08@gmail.com>
2023-01-26 17:58:26 -0300

    Function delete task and delete all task implemented

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+//Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    {
+      task: "Minha nova task",
+      checked: true
+    },
+    {
+      task: "Minha nova task 2",
+      checked: false
+    }
+  ]
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  public deleteItemTask(index: number){
+    this.taskList.splice(index, 1);
+  }
+
+  public deleteAllTask(){
+    const confirm = window.confirm("Você realmente deseja deletar todas as tarefas?");
+    if(confirm){
+      this.taskList = [];
+    }
+  }
 }
